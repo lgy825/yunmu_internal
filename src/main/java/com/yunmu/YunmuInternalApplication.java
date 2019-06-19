77849ccb418c5c63e8ff62fea26028e46da6ed37
lgy <1354449188@qq.com>
2019-06-19 21:18:47 +0800

    错误修改

--- src/main/java/com/yunmu/YunmuInternalApplication.java
+++ src/main/java/com/yunmu/YunmuInternalApplication.java
@@ -1,9 +1,13 @@
 package com.yunmu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.yunmu.back","com.yunmu.bapp","com.yunmu.core"})
+@MapperScan(basePackages = {"com.yunmu.core.dao"})
 public class YunmuInternalApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/yunmu/core/dao/hourse/HourseTypeMapperExt.java
+++ src/main/java/com/yunmu/core/dao/hourse/HourseTypeMapperExt.java
@@ -1,9 +1,6 @@
 package com.yunmu.core.dao.hourse;
 
-import com.yunmu.core.model.hourse.HourseType;
-import com.yunmu.core.model.hourse.HourseTypeExample;
 import com.yunmu.core.model.hourse.HourseTypeExt;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
